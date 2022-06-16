fe49f286e4026f92caed7bec0daa8aab14810935
Frans08 <fransfebry24@gmail.com>
2022-06-16 22:42:49 +0700

    tugas react

---
+++ .github/workflows/Materi/Komponen/Pembahasan/functionalComponent.jsx
@@ -0,0 +1,11 @@
+const FunctionalComponent = () => {
+return (
+    <div>
+        <h1>Membuat komponen dengan functional component</h1>
+        <h2>Hello Frans</h2>
+    </div>
+
+)
+
+}
+export default FunctionalComponent;
---
+++ .github/workflows/Materi/var angka;.js
@@ -0,0 +1,10 @@
+var angka;
+var kesempatan;
+var tebakan;
+
+angka=Math.random();
+tabakan=prompt("Masukan Tebakan anda dari angka 1-3 (kesempatan = 3 kali")
+
+if (angka > 1) {
+    tebakan
+}
